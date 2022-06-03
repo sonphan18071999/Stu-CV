a8c708281a39a519435b7ddd4938a8eb4e2625b2
sonphan18071999@gmail.com <sonphan18071999@gmail.com>
2022-06-03 13:55:34 +0700

    Models created

---
+++ src/models/UserInformation.ts
@@ -0,0 +1,9 @@
+export default interface UserInformation{
+    fistName: string
+    lastName: string
+    title: string
+    email: string
+    phone: string
+    website?: string
+    location?: string
+}
